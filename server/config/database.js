022257d66fb492bb26f0c888de4583ba25369c50
lin <lin.ferr@mail.ru>
2026-03-16 22:54:31 +0500

    не баг а фича

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,19 @@
 import { Sequelize } from 'sequelize';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const dbPath = process.env.NODE_ENV === 'production'
+  ? '/var/data/database.sqlite'  // Путь для Render (позже настроим диск)
+  : path.join(__dirname, './database.sqlite');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false,
+  storage:  dbPath,
+  logging: process.env.NODE_ENV !== 'production',
   define: {
     timestamps: true,
     underscored: true
