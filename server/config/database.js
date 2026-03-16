888fb7b5647e5039131f51914f45404a97598f29
lin <lin.ferr@mail.ru>
2026-03-16 23:23:49 +0500

    yoyy

--- server/config/database.js
+++ server/config/database.js
@@ -1,23 +1,30 @@
 import { Sequelize } from 'sequelize';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+let dbPath;
 
-const dbPath = process.env.NODE_ENV === 'production'
-  ? '/var/data/database.sqlite'  // Путь для Render (позже настроим диск)
-  : path.join(__dirname, './database.sqlite');
+if (process.env.NODE_ENV === 'production') {
+  const dataDir = path.join(__dirname, '../../data');
+  
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+  }
+  
+  dbPath = path.join(dataDir, 'database.sqlite');
+  console.log('📁 Продакшен БД путь:', dbPath);
+} else {
+  dbPath = path.join(__dirname, '../../database.sqlite');
+}
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage:  dbPath,
-  logging: process.env.NODE_ENV !== 'production',
-  define: {
-    timestamps: true,
-    underscored: true
-  }
+  storage: dbPath,
+  logging: process.env.NODE_ENV !== 'production'
 });
 
 export default sequelize;
