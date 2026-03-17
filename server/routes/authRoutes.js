09dab67a490e382d919f78203740d99b87407646
lin <lin.ferr@mail.ru>
2026-03-17 10:42:43 +0500

    may

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { register, login, logout, checkAuth } from '../controllers/authController.js';
 
-
 const router = express.Router();
 
-router.post('/register', authLimiter, register);
-router.post('/login', authLimiter, login);
+// 🟢 Убрали authLimiter
+router.post('/register', register);
+router.post('/login', login);
 router.post('/logout', logout);
 router.get('/check', checkAuth);
 
